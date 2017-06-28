bb5caa60e4da30b3ae0cbba5e6c219a409a66ac3
E5000235 <ArunNayak@IGDCLT0108.local>
2017-06-28 18:27:07 +0530

    changes

--- src/app/devicepage/devicepage.component.ts
+++ src/app/devicepage/devicepage.component.ts
@@ -14,10 +14,18 @@ import { RouterModule, Routes } from '@angular/router';
 export class DevicepageComponent {
     public label : string = '';
     public fieldType : string = '';
+    public tableName : string = '';
+    public fieldName : string = '';
+    public uiPosition : string = '';
+    public styles : string = '';
 
     constructor() {
-      this.label = 'label';
-      this.fieldType = 'Field type';
+        this.label = 'label';
+        this.fieldType = 'Field type';
+        this.tableName = 'table name'
+        this.fieldName = 'field name'
+        this.uiPosition = 'UI position'
+        this.styles = 'styles'
     }
 
     receivedData: Array<any> = [];
@@ -28,60 +36,45 @@ export class DevicepageComponent {
       "name": "button_name", 
       "id": 1, 
       "class": "button-class", 
-      "imgUrl": "../../assets/img/btn.png",
-      "label": "Label",
-      "fieldType": "Text field",
-      "tableName": "Table name",
-      "feildName": "Field name",
-      "uiPosition": "UI Position",
-      "style": "Styles",
-       "form": [
-          {
-              "label": "Label",
-              "fieldType": "Text field",
-              "tableName": "Table name",
-              "feildName": "Field name",
-              "uiPosition": "UI Position",
-              "style": "Styles"
-          }
-    ]},
+      "imgUrl": "../../assets/img/btn.png"
+       },
     { 
       "type":"text",
       "name": "text_name", 
       "id": 2, 
       "class": "text-class", 
       "description": "description2", 
-      "imgUrl": "../../assets/img/txt-input.png",
-      "label": "Label",
-      "fieldType": "Text field",
-      "tableName": "Table name",
-      "feildName": "Field name",
-      "uiPosition": "UI Position",
-      "style": "Styles",
-       "form": [
-          {
-              "label": "Label",
-              "fieldType": "Text field",
-              "tableName": "Table name",
-              "feildName": "Field name",
-              "uiPosition": "UI Position",
-              "style": "Styles"
-          }
-      
-    ]}
+      "imgUrl": "../../assets/img/txt-input.png"
+    }
 ]
 
     transferDataSuccess($event: any) {
         this.receivedData.push($event);
+        console.log(this.receivedData.length);
     }
   
     addLabelvalue(value:any){
-        console.log(this.receivedData[0].dragData.type == 'button');
         this.receivedData[0].dragData.label = value;
     }
 
     addFieldTypevalue(value:any){
         this.receivedData[0].dragData.fieldType = value;
+    }
+
+    addTableNamevalue(value:any){
+        this.receivedData[0].dragData.tableName = value;
+    }
+
+     addFieldNamevalue(value:any){
+        this.receivedData[0].dragData.fieldName = value;
+    }
+
+     addUIPositionvalue(value:any){
+        this.receivedData[0].dragData.uiPosition = value;
+    }
+
+     addStylesvalue(value:any){
+        this.receivedData[0].dragData.styles = value;
     }
 
     downloadJSON (){
